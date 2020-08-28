046ab0bcf930f556be419e279c8116dd18a75f7d
Aman Chaudhary <aman28111998@gmail.com>
2020-08-28 21:21:33 +0530

    first

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const newsapi = new NewsAPI('00d1f76d27e64f0496888c9da6cbd11c');
 
 app.get('/',(req, res, next) => {
     newsapi.v2.everything({
-        q:'indore gas',
+        q:'pet ki gas',
         id:'the-times-of-india',
         language:'en',
         domain:'financial'
